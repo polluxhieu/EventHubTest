07a998bfce6eb699d5a9a3ede57e1cd131572bc2
Hieu Tran <hieut@qssolutions.nl>
2018-10-19 17:14:47 +0700

    Add

---
+++ EventsReceiver/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace EventsReceiver
+{
+
+    class Program
+    {
+        private static IConfigurationRoot _config;
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+            _config = builder.Build();
+
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventHubConnection = _config["EventHubConnection"];
+            var eventHubName = _config["EventHubName"];
+            var storageContainerName = _config["StorageContainerName"];
+            var storageAccountName = _config["StorageAccountName"];
+            var storageAccountKey = _config["StorageAccountKey"];
+            var storageConnectionString = _config["StorageConnectionString"];
+
+            var eventProcessorHost = new EventProcessorHost(eventHubName, PartitionReceiver.DefaultConsumerGroupName, eventHubConnection, storageConnectionString, storageContainerName);
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
---
+++ EventsSender/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Extensions.Configuration;
+
+namespace EventsSender
+{
+    internal class Program
+    {
+        private static IConfigurationRoot _config;
+        private static EventHubClient _client;
+
+        private static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+            _config = builder.Build();
+
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var eventHubConnection = _config["EventHubConnection"];
+            var eventHubName = _config["EventHubName"];
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(eventHubConnection) { EntityPath = eventHubName };
+            _client = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessageToEventHub(5);
+            await _client.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        private static async Task SendMessageToEventHub(int number)
+        {
+            for (int i = 0; i < number; ++i)
+            {
+                try
+                {
+                    var msg = $"Message {i}";
+                    Console.WriteLine($"Sending {msg}");
+                    await _client.SendAsync(new EventData(Encoding.UTF8.GetBytes(msg)));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{DateTime.Now} EXCEPTION: {ex.Message}");
+                }
+                await Task.Delay(10);
+            }
+
+            Console.WriteLine($"{number} messages sent.");
+        }
+    }
+}
